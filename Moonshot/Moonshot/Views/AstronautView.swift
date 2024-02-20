b8a01a8c513c72d1ea292491d5aa7d9ca86e26eb
Iñaki <inayabarb@gmail.com>
2024-02-20 15:09:28 -0300

    Day 41: Moonshot app

---
+++ Moonshot/Moonshot/Views/AstronautView.swift
@@ -0,0 +1,18 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Iñaki Yabar Bilbao on 20/02/2024.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AstronautView()
+}
