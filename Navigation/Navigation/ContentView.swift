09bc13963ef552ea2762c643c8b10ff18933f0d7
Iñaki <inayabarb@gmail.com>
2024-02-21 12:44:50 -0300

    .navigationDestination

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(0..<100) { i in
+                NavigationLink("Select \(i)", value: i)
+            }
+            .navigationDestination(for: Int.self, destination: {id in Text("Some \(id)")})
         }
-        .padding()
     }
 }
 
